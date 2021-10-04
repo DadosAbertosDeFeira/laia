db5e692c12507901be1b0ea179ca284a614e5170
turquetti <gabrielaturquetti@gmail.com>
2021-10-04 18:50:39 -0300

    altera import.py

--- information_requests/management/commands/import.py
+++ information_requests/management/commands/import.py
@@ -1,8 +1,9 @@
 import csv
-from django.core.management.base import BaseCommand
-from information_requests.models import InformationRequest
-from information_requests.models import PublicAgency
+
 from dateutil.parser import parse
+from django.core.management.base import BaseCommand
+
+from information_requests.models import InformationRequest, PublicAgency
 
 
 class Command(BaseCommand):
@@ -17,23 +18,28 @@ class Command(BaseCommand):
 
             for row in reader:
                 public_agency, _ = PublicAgency.objects.get_or_create(
-                    name = row["Nome do Órgão"],
-                    initials = row["Sigla do Órgão"],
-                    sphere = row["Esfera"].lower()
+                    name=row["Nome do Órgão"],
+                    initials=row["Sigla do Órgão"],
+                    sphere=row["Esfera"].lower(),
                 )
-                
+
                 InformationRequest.objects.get_or_create(
-                    sent_at = parse(row["Data de envio"]),
-                    replied_at = parse(row["Data de resposta"]) if row["Data de resposta"] else None,
-                    public_agency = public_agency,
-                    title = row["Pedido"],
-                    contact = row["Meio de contato"],
-                    status = row["Status"].lower().replace(" ", "_"),
-                    historic = row["Histórico"],
-                    text = row["Texto"],
-                    reply = row["Resposta"],
+                    sent_at=parse(row["Data de envio"]),
+                    replied_at=parse(row["Data de resposta"])
+                    if row["Data de resposta"]
+                    else None,
+                    public_agency=public_agency,
+                    title=row["Pedido"],
+                    contact=row["Meio de contato"],
+                    status=row["Status"].lower().replace(" ", "_"),
+                    historic=row["Histórico"],
+                    text=row["Texto"],
+                    reply=row["Resposta"],
                 )
                 count += 1
 
-            self.stdout.write(self.style.SUCCESS(f"Concluído! Foram inseridos um total de {count} pedidos de informação."))
-            
+            self.stdout.write(
+                self.style.SUCCESS(
+                    f"Concluído! Foram inseridos {count} pedidos de informação."
+                )
+            )
