781a0815de65cabd776ada488d023111761f09ae
turquetti <gabrielaturquetti@gmail.com>
2021-10-15 15:32:50 -0300

    adiciona verbose name no apps.py

--- information_requests/apps.py
+++ information_requests/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class InformationRequestsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "information_requests"
+    verbose_name = "Pedidos"
