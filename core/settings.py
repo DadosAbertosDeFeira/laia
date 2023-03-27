2c78a5576fa40f6a8a4d8e856bc0e88d5a44a93d
Ana Paula Gomes <apgomes88@gmail.com>
2023-03-27 17:28:43 +0200

    Corrige conexão com o banco no GA

--- core/settings.py
+++ core/settings.py
@@ -100,3 +100,6 @@ STATIC_URL = "/static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 ORGANIZATION_NAME = config("ORGANIZATION_NAME")
+
+if os.environ.get("CI"):
+    del DATABASES["default"]["OPTIONS"]["sslmode"]
