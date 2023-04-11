f4b36ac8f626e98fd86f5f4b5afdec49ca19674a
Ana Paula Gomes <1899950+anapaulagomes@users.noreply.github.com>
2023-04-11 21:43:27 +0200

    Configura sentry (#67)
    * Configura sentry
    
    * fixa versão do poetry
    
    * Altera versão do python
    
    * atualiza versão do python no dockerfile
    
    * run isort to check files
    
    * Update python to 3.8 and sentry-sdk
    
    * Atualiza versões no Dockerfile do Python e do poetry
    
    * Pula configuração de db do Heroku (a ser removida em breve)
    
    * Atualiza versão do poetry no CI também
    
    ---------
    
    Co-authored-by: Luanda Dantas Leite <luanda.dantas@MacBook-Pro-de-Luanda.local>

--- core/settings.py
+++ core/settings.py
@@ -1,10 +1,19 @@
+import os
 from pathlib import Path
 
 import dj_database_url
 import django_on_heroku
+import sentry_sdk
 from decouple import config
+from sentry_sdk.integrations.django import DjangoIntegration
 
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+sentry_sdk.init(
+    dsn=os.getenv("SENTRY_DSN"),
+    integrations=[DjangoIntegration()],
+    traces_sample_rate=1.0,
+)
 
 SECRET_KEY = config("SECRET_KEY")
 
@@ -93,4 +102,4 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 ORGANIZATION_NAME = config("ORGANIZATION_NAME")
 
-django_on_heroku.settings(locals())
+django_on_heroku.settings(locals(), databases=False)
